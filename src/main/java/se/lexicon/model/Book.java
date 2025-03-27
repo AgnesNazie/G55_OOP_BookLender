18b7f2c921a2840a39725a4eebe9c85bbb683379
AgnesNazie <cheagnesmbo@gmail.com>
2025-03-27 15:29:15 +0100

    updated person and book class to handle exceptions

--- src/main/java/se/lexicon/model/Book.java
+++ src/main/java/se/lexicon/model/Book.java
@@ -8,7 +8,6 @@ import java.util.UUID;
  * to manage book-related information and operations.
  */
 public class Book {
-    // todo: needs completion
     // create fields for book class
 
     private String id;
@@ -19,6 +18,9 @@ public class Book {
     //creates constructors for book class
 
     public Book(String title, String author) {
+        if (title == null || title.trim().isEmpty() || author == null || author.trim().isEmpty()) {
+            throw  new IllegalArgumentException("Title and author cannot be empty.");
+        }
         this.id = generateBookId();
         this.title = title;
         this.author = author;
@@ -34,15 +36,23 @@ public class Book {
     }
 
     //create setters
-    public void SetAvailable(boolean available) {
+    public void setAvailable(boolean available) {
         this.available = available;
     }
 
     // create getters
     public String getId() {
-
+        return id;
     }
-
+    public String getTitle() {
+        return title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public boolean isAvailable() {
+        return available;
+    }
 
     private String generateBookId() {
         return "B-" + UUID.randomUUID().toString();
--- src/main/java/se/lexicon/model/Person.java
+++ src/main/java/se/lexicon/model/Person.java
@@ -15,30 +15,62 @@ public class Person {
     // create constructors for person class
 
     public Person(String firstName, String lastName) {
+        // throw exception to handel missing name
+        if (firstName == null || firstName.trim().isEmpty() || lastName == null || lastName.trim().isEmpty()) {
+            throw new IllegalArgumentException("first name  cannot be empty");
+        }
         this.id = ++sequencer;
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
-    // create getters for nextId
+    //create getters
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    // create method for nextId
 
     public static int getNextId() {
         return sequencer + 1;
     }
 
-    public void loanBook(se.lexicon.Book book) {
-        if (book.isAvailable()) {
-            book.setAvailable(false);
-            System.out.println(firstName + " " + lastName + "borrowed the book " + book.getTitle());
-        } else {
-            System.out.println("The book is already borrowed. ");
+    public void loanBook(Book book) {
+        //prevent book from  being default (null)
+        if (book == null) {
+            throw new IllegalArgumentException("Book cannot be null.");
         }
-
+        // prevent borrowing unavailable book
+        if (!book.isAvailable()) {
+            throw new IllegalStateException("This book is already borrowed.");
+        }
+        book.setAvailable(false);
+        System.out.println(firstName + " " + lastName + "borrowed the book " + book.getTitle());
     }
 
     public void returnBook(Book book) {
+        //prevent borrowing returning book that was not borrowed
+        if (book == null) throw new IllegalArgumentException("Book cannot be null.");
+
+        if (book.isAvailable())
+            throw new IllegalStateException("This book was not borrowed");
+
         book.setAvailable(true);
         System.out.println(firstName + " " + lastName + "returned the book " + book.getTitle());
+
     }
+
+    public String getPersonInformation() {
+        return "ID: " + id + ", Name: " + firstName + " " + lastName;
+    }
+
 }
 
